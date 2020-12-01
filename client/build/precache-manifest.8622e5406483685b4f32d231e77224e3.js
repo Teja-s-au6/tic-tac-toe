3c71cb14431f5a24c9d0c08fa5f203a29c652822
tejaattainu <tejspvg@gmail.com>
2020-12-01 21:52:56 +0530

    Multiplayer added

---
+++ client/build/precache-manifest.8622e5406483685b4f32d231e77224e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a711016b482620d0f09b1ad062ed32fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b213b5975e88b5e5e88",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "e873f4cd3874833fb607",
+    "url": "/static/css/main.e86ccbb2.chunk.css"
+  },
+  {
+    "revision": "4b213b5975e88b5e5e88",
+    "url": "/static/js/2.025b827a.chunk.js"
+  },
+  {
+    "revision": "4b6ddcc7458664c42c0d6c9ea41a632f",
+    "url": "/static/js/2.025b827a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e873f4cd3874833fb607",
+    "url": "/static/js/main.344286e4.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
